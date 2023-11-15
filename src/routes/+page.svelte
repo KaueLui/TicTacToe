9f77a624e062d00ffe141efe4a91c912dcd7f96b
Kauê Luí <ofckaue@gmail.com>
2023-11-15 20:57:38 -0300

    First configuration files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="../game">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    let buttons = new Array(9).fill(null);
+    let result = null;
+    let turn = 'X';
+
+    function setValue(i){
+        buttons[i] = turn;
+        buttons = [...buttons];
+        turn = turn == 'X'?'O' : 'X';
+    }
+</script>
+    
+<style>
+    .tictac {
+        width: 300px;
+        height: 300px;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .tictac button {
+        width: 100px;
+        height: 100px;
+        margin: 0;
+    }
+</style>
+
+
+{#if !result}
+    <div class="tictac">
+        {#each buttons as button,i}
+        <button on:click={( )=>{setValue(i)}}>
+            {button?button:""}
+        </button>
+        {/each}
+
+    </div>
+
+    {:else}
+
+    <div>
+
+
+    </div>
+{/if}
